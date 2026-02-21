5e358985173f4f1f3e89bd48c0c03733cdc3c27e
yujioyama <yujioyamanyc@gmail.com>
2026-02-22 08:26:08 +0900

    update nodemailer transport service to use smtp.gmail.com

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -54,7 +54,7 @@ export class AuthService {
     });
 
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      service: "smtp.gmail.com",
       port: 587,
       secure: false,
       auth: {
